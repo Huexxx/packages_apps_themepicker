1e4fe9f68241be34665adac29974cc99d550fc80
Huexxx <huexxx@gmail.com>
2023-01-19 00:51:34 +0100

    ThemePicker: Hide icon pack selection section

--- src/com/android/customization/module/DefaultCustomizationSections.java
+++ src/com/android/customization/module/DefaultCustomizationSections.java
@@ -11,8 +11,8 @@ import com.android.customization.model.font.FontManager;
 import com.android.customization.model.font.FontSectionController;
 import com.android.customization.model.grid.GridOptionsManager;
 import com.android.customization.model.grid.GridSectionController;
-import com.android.customization.model.iconpack.IconPackManager;
-import com.android.customization.model.iconpack.IconPackSectionController;
+//import com.android.customization.model.iconpack.IconPackManager;
+//import com.android.customization.model.iconpack.IconPackSectionController;
 import com.android.customization.model.mode.DarkModeSectionController;
 import com.android.customization.model.theme.OverlayManagerCompat;
 import com.android.customization.model.themedicon.ThemedIconPackSectionController;
@@ -71,9 +71,9 @@ public final class DefaultCustomizationSections implements CustomizationSections
         sectionControllers.add(new GridSectionController(
                 GridOptionsManager.getInstance(activity), sectionNavigationController));
 
-        // Icon pack selection section.
+/*        // Icon pack selection section.
         sectionControllers.add(new IconPackSectionController(
-                IconPackManager.getInstance(activity, new OverlayManagerCompat(activity)), sectionNavigationController));
+                IconPackManager.getInstance(activity, new OverlayManagerCompat(activity)), sectionNavigationController));*/
 
         // Font selection section.
         sectionControllers.add(new FontSectionController(
